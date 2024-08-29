4a2d649c2bab06da452de4fb1d63dc7572acc393
Johannes Ballmann <gitlab@jball.de>
2024-08-29 18:13:38 +0200

    bump kotlin/dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	kotlin("jvm") version "2.0.0"
-	kotlin("plugin.spring") version "1.9.24"
-	id("org.springframework.boot") version "3.3.2"
+	kotlin("jvm") version "2.0.20"
+	kotlin("plugin.spring") version "2.0.20"
+	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("openapi-module-generate")
 }
